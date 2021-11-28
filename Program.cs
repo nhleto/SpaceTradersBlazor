1667d41266ce9868622d0eec547dd7b7924877d2
Henry Leto <hnr.leto11@gmail.com>
2021-11-28 17:57:30 -0500

    sus out http client

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 using SpaceTradersBlazor;
+using SpaceTradersBlazor.Models;
+using SpaceTradersBlazor.SpaceTradersClient;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,4 +11,11 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
+
+var spaceTradersConfig = builder.Configuration.GetSection("SpaceTraders").Get<SpaceTradersConfig>();
+builder.Services.AddHttpClient<SpaceTradersClient>(client =>
+{
+    client.BaseAddress = new Uri(spaceTradersConfig.StartupUrl);
+});
+
 await builder.Build().RunAsync();
